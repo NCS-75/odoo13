e8f1443638865dbb5178d5af38d07ae7652406c4
DefferrardJ <jeremy.defferrard@prisme.ch>
2019-11-22 15:01:59 +0100

    Commit

--- ons_productivity_sale_layout/models/__init__.py
+++ ons_productivity_sale_layout/models/__init__.py
@@ -19,5 +19,5 @@
 #
 ##########################################################################
 from . import layout
-from . import account_move
-from . import sale_order
+from . import account_move_line
+from . import sale_order_line
