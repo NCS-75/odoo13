f64ba12ac2ed846bcee8784f4a162986b946b696
jeremyd <jeremy.defferrard@prisme.ch>
2020-05-04 15:57:49 +0200

    Update layout.py

--- ons_productivity_sale_layout/models/layout.py
+++ ons_productivity_sale_layout/models/layout.py
@@ -30,12 +30,12 @@ LAYOUTS_LIST = [
 def layout_val_2_text(layout_type):
     val = _( 'Product' )
     if layout_type == 'subtotal':
-		val = _( 'Sub Total' )
-	elif layout_type == 'line':
-		val = _( 'Separator Line' )
+        val = _( 'Sub Total' )
+    elif layout_type == 'line':
+        val = _( 'Separator Line' )
     elif layout_type == 'break':
-		val = _( 'Page Break' )
-		
+        val = _( 'Page Break' )
+
     return val
 
 
