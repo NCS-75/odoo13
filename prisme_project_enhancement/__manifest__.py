f1fc20b53f2cc54cca95716b5002d39bd129ac55
Hugo Buchs <hugo.buchs@prisme.ch>
2020-05-09 16:47:44 +0200

    added module files

---
+++ prisme_project_enhancement/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#######################################
+# Prisme Solutions Informatique SA    #
+#######################################
+# Author: Hugo Buchs                  #
+# Email:  hugo.buchs@prisme.ch        #
+# Web:    www.prisme.ch               #
+# Date:   09.05.2020                  #
+#######################################
+{
+    'name': 'Prisme Project Enhancement',
+    'version': '2019-05-09 00:00',
+    'category': 'Tools',
+    'summary': "Adds an 'amount', a 'balance_previous_command' and an 'account' field to the Tasks of the Projects application. It also adds a 'project_id' field to the invoices.",
+    'description': """
+        This module adds an 'amount', a 'balance_previous_command' and an 'account' field to the Tasks of the Projects application.
+        It also adds a 'project_id' field to the invoices.
+    """,
+    'author': 'Prisme Solutions Informatique SA',
+    'website': 'https://www.prisme.ch',
+    'depends': [
+      'project',
+      'account'
+    ],
+    'data': [
+        'views/views.xml'
+    ],
+    'demo': [
+    ],
+    'css': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ prisme_project_enhancement/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#######################################
+# Prisme Solutions Informatique SA    #
+#######################################
+# Author: Hugo Buchs                  #
+# Email:  hugo.buchs@prisme.ch        #
+# Web:    www.prisme.ch               #
+# Date:   09.05.2020                  #
+#######################################
+from . import task
+from . import invoice
