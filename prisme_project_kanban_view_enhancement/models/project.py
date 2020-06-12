762424e21278be0ccee2ceef701b5acf8121bce3
jeremyd <jeremy.defferrard@prisme.ch>
2020-06-12 13:47:16 +0200

    Update project.py

--- prisme_project_kanban_view_enhancement/models/project.py
+++ prisme_project_kanban_view_enhancement/models/project.py
@@ -18,16 +18,23 @@
 #
 ##########################################################################
 
-from odoo import models, api, fields, _
+from odoo import models, api, fields
 
 class Project(models.Model):
     _inherit = 'project.project'
     prisme_sequence = fields.Char('Prisme Analytic Account Sequence', related='analytic_account_id.prisme_sequence')
 
+
+    @api.onchange('customer_id')
+    def _check_change(self):
+        self.analytic_account_id.customer_id=self.customer_id
+
     @api.model
     def create(self, vals):
-        vals_account = {'name': vals.get("name"),
-                        'prisme_sequence': (self.env['ir.sequence'].next_by_code('prisme.analytic.account.sequence') or 0)}
-        analytic_account= self.env['account.analytic.account']
-        vals['analytic_account_id'] = analytic_account.create(vals_account).id
+        if not vals['analytic_account_id']:
+            vals_account = {'name': vals.get("name"),
+                            'prisme_sequence': (self.env['ir.sequence'].next_by_code('prisme.analytic.account.sequence') or 0),
+                            'customer_id': vals['customer_id']}
+            analytic_account= self.env['account.analytic.account']
+            vals['analytic_account_id'] = analytic_account.create(vals_account).id
         return super(Project, self).create(vals)
