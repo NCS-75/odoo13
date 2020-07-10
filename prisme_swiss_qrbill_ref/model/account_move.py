a439a51685b7cc4ef82f754c57a2531bfb712a3d
davidp <david.progin@prisme.ch>
2020-07-10 13:50:05 +0200

    Updated QR Reference generation

--- prisme_swiss_qrbill_ref/model/account_move.py
+++ prisme_swiss_qrbill_ref/model/account_move.py
@@ -15,8 +15,8 @@
 #    You should have received a copy of the GNU Affero General Public Lic
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    Project ID :    
-#    Phabricator :   
+#    Project ID :    OERP-002-10
+#    Phabricator :   T738
 #
 ##########################################################################
 from odoo import models
@@ -30,41 +30,51 @@ class account_move(models.Model):
     def _compute_qr_ref(self):
         for record in self:
             qr_reference = ''
-            if record.invoice_partner_bank_id and record.type == 'out_invoice':
+            # First we check if this is an outgoing invoice and if a bank account has been provided
+            if record.type == 'out_invoice' and record.invoice_partner_bank_id:
+                
+                # We need to generate the QR reference only if the bank account has a QR IBAN
                 if record.invoice_partner_bank_id._is_qr_iban():
-                    invoice_number = ''
                     
-                    # We get the invoice number and remove all non digit characters for qr_ref
-                    try:
+                    # If an ISR reference has been generated, we use it as our QR reference since it's the same structure and data.
+                    # Used for "compatibility" between ISR and QR.
+                    if record.l10n_ch_isr_number:
+                        qr_reference = record.l10n_ch_isr_number
+                    
+                    # Otherwise we generate the QR reference with the same structure as ISR -> ISR subscription number as first digits and invoice number as last digits.
+                    else:
+                        invoice_number = ''
+                        
+                        # We get the invoice number and get only digital characters
                         if record.name:
                             for character in record.name:
                                 if character.isdigit():
                                     invoice_number += character
-                    except:
-                        None
                         
-                    if invoice_number:
-                        
-                        issuer_number = record.invoice_partner_bank_id.l10n_ch_postal
-                        
-                        # we check if the invoice_issuer_number exists and contains only digits
-                        if issuer_number and issuer_number.isdigit():
+                        # If the invoice number actually had digital characters, we proceed to the QR reference generation
+                        if invoice_number:
                             
-                            # adding the issuer number at the beginning of the qr_reference and the invoice_internal_ref at the end. Padding with 0 in between so the length equals 26
-                            qr_reference_no_check_digit = issuer_number + invoice_number.rjust(QR_REF_LENGTH - 1 - len(issuer_number), '0')
+                            issuer_number = record.invoice_partner_bank_id.l10n_ch_postal
                             
-                        else:
-                            # creating the qr_reference with no invoice_issuer_number at the beginning. Padding with 0 so the length equals 26
-                            qr_reference_no_check_digit = invoice_number.rjust(QR_REF_LENGTH - 1, '0')
-                    
-                        # Setting the qr_reference field with the qr_reference previously created and the check digit at the end
-                        qr_reference = mod10r(qr_reference_no_check_digit)
+                            # We check if the invoice_issuer_number exists and contains only digits
+                            if issuer_number and issuer_number.isdigit():
+                                
+                                # Adding the issuer number at the beginning of the qr_reference and the invoice_internal_ref at the end. Padding with 0 in between so the length equals 26
+                                qr_reference_no_check_digit = issuer_number + invoice_number.rjust(QR_REF_LENGTH - 1 - len(issuer_number), '0')
+                                
+                            else:
+                                # Creating the qr_reference with no invoice_issuer_number at the beginning. Padding with 0 so the length equals 26
+                                qr_reference_no_check_digit = invoice_number.rjust(QR_REF_LENGTH - 1, '0')
+                        
+                            # Adding the check digit to the end of the QR reference
+                            qr_reference = mod10r(qr_reference_no_check_digit)
             
             return qr_reference
-            
+    
     def _get_invoice_reference_odoo_invoice(self):
-        self.ensure_one()
+        """Returns the result of _compute_qr_ref if a value has been returned, or calls super otherwise."""
+        
         reference = self._compute_qr_ref()
         if not reference:
-            reference = self.name
+            reference = super(account_move)._get_invoice_reference_odoo_invoice()
         return reference
