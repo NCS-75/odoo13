81de32e1866016f6cebde3b5f5d9f51e749ea3ab
Hugo Buchs <hugo.buchs@prisme.ch>
2020-07-29 18:05:09 +0200

    Fixed double username in invoice line

--- prisme_invoicing_timesheet/models/account_analytic_line.py
+++ prisme_invoicing_timesheet/models/account_analytic_line.py
@@ -194,17 +194,17 @@ class prisme_account_analytic_line(models.Model):
 
             note = []
             # start prisme modification
-            alreadyadded=False
+            #alreadyadded=False
             for line in analytic_lines:
-                # Get user
-                cr = self.env.cr
-                cr.execute("select p.name from res_partner p, res_users u where p.id=u.partner_id and u.id="+str(user_id))
-                usr=cr.fetchall()
-                # set invoice_line_note
-                details = []
-                if not(alreadyadded):
-                    details.append("- "+usr[0][0])
-                alreadyadded=True
+            #    # Get user
+            #    cr = self.env.cr
+            #    cr.execute("select p.name from res_partner p, res_users u where p.id=u.partner_id and u.id="+str(user_id))
+            #    usr=cr.fetchall()
+            # set invoice_line_note
+            #    details = []
+            #    if not(alreadyadded):
+            #        details.append("- "+usr[0][0])
+            #    alreadyadded=True
             # end prisme modification
                 if data.get('date', False):
                     details.append(line['date'])
