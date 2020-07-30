8deb38d4b3dfc62896573b47d059ac10e85b260d
Hugo Buchs <hugo.buchs@prisme.ch>
2020-07-30 16:25:56 +0200

    Cleaned module

--- prisme_invoicing_timesheet_report/__manifest__.py
+++ prisme_invoicing_timesheet_report/__manifest__.py
@@ -1,8 +1,8 @@
 {
-'name': 'Prisme Timesheet Enhancement',
+'name': 'Prisme Invoicing Timesheet Report',
 'version': '2017-12-01 16:24',
 'category': 'Human Resources',
-'summary': 'start / end time, reports',
+'summary': 'Report for invoiced timesheets',
 'author': 'Prisme Solutions Informatique SA',
 'website': 'http://www.prisme.ch',
 'depends': [
@@ -10,10 +10,7 @@
         ],
 'init_xml': [],
     'update_xml': [
-        #'report/templates/hr_timesheet_sheet_check.xml',
-        #'report/templates/account_analytic_line.xml',
         'report/templates/account_invoice_detail.xml',
-        #'security/ir.model.access.csv',
     ],
     'demo_xml': [],
     'test': [],
